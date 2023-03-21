7b23638f41d94a80c78c5efe7005c04c06b3c11b
AdaN6 <a_disney_da@hotmail.com>
2023-03-21 12:04:12 +0100

    created multiple stories for one component

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -17,3 +17,30 @@ Red.args ={
 }
 
 
+export const Green = Template.bind({})
+Green.args ={
+    backgroundColor: 'green',
+    label: "Press Me",
+    size:"md",
+}
+
+export const Small = Template.bind({})
+Small.args ={
+    backgroundColor: 'red',
+    label: "Press Me",
+    size:"sm",
+}
+
+export const Large = Template.bind({})
+Large.args ={
+    backgroundColor: 'red',
+    label: "Press Me",
+    size:"lg",
+}
+
+export const LongLabel = Template.bind({})
+LongLabel.args ={
+    backgroundColor: 'red',
+    label: "This is super long label",
+    size:"md",
+}
